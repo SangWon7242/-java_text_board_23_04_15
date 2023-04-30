126f36e1d37d3be4f7ee70bcfd6aa2eba6df7f3f
SangWon7242 <gotkdqja@gmail.com>
2023-04-30 12:19:15 +0900

    actionUsrArticleDetail

--- src/com/sbs/exam/board/Main.java
+++ src/com/sbs/exam/board/Main.java
@@ -42,32 +42,7 @@ public class Main {
        articleLastId++;
       }
       else if(rq.getUrlPath().equals("/usr/article/detail")) {
-        if(params.containsKey("id") == false) {
-          System.out.println("id를 입력해주세요.");
-          continue;
-        }
-
-        int id = 0;
-
-        try {
-          id = Integer.parseInt(params.get("id"));
-        }
-        catch (NumberFormatException e) {
-          System.out.println("id를 정수 형태로 입력해주세요.");
-          continue;
-        }
-
-        if(id > articles.size()) {
-          System.out.println("게시물이 존재하지 않습니다.");
-          continue;
-        }
-
-        Article article = articles.get(id - 1);
-
-        System.out.println("== 게시물 상세보기 ==");
-        System.out.printf("번호 : %d\n", article.id);
-        System.out.printf("제목 : %s\n", article.title);
-        System.out.printf("내용 : %s\n", article.body);
+        actionUsrArticleDetail(rq, articles);
       }
       else {
         System.out.printf("입력된 명령어 : %s\n", cmd);
@@ -77,6 +52,37 @@ public class Main {
     System.out.println("== 프로그램 종료 ==");
 
     sc.close();
+  }
+
+  private static void actionUsrArticleDetail(Rq rq, List<Article> articles) {
+    Map<String, String> params = rq.getParams();
+
+    if(params.containsKey("id") == false) {
+      System.out.println("id를 입력해주세요.");
+      return;
+    }
+
+    int id = 0;
+
+    try {
+      id = Integer.parseInt(params.get("id"));
+    }
+    catch (NumberFormatException e) {
+      System.out.println("id를 정수 형태로 입력해주세요.");
+      return;
+    }
+
+    if(id > articles.size()) {
+      System.out.println("게시물이 존재하지 않습니다.");
+      return;
+    }
+
+    Article article = articles.get(id - 1);
+
+    System.out.println("== 게시물 상세보기 ==");
+    System.out.printf("번호 : %d\n", article.id);
+    System.out.printf("제목 : %s\n", article.title);
+    System.out.printf("내용 : %s\n", article.body);
   }
 
   private static void actionUsrArticleWrite(Scanner sc, List<Article> articles, int articleLastId) {
