eaa2d2cd11931eed0e564db812b0c8221ed25f4e
SangWon7242 <gotkdqja@gmail.com>
2023-05-06 12:28:37 +0900

    UsrArticleController 도입

--- src/com/sbs/exam/board/Article.java
+++ src/com/sbs/exam/board/Article.java
@@ -1,10 +1,10 @@
 package com.sbs.exam.board;
 public class Article {
-  int id;
-  String title;
-  String body;
+  public int id;
+  public String title;
+  public String body;
 
-  Article(int id, String title, String body) {
+  public Article(int id, String title, String body) {
     this.id = id;
     this.title = title;
     this.body = body;
